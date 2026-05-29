5f4cf39a003b4a017ce0d06a8aa6aa952f379fda
shivansh pareek <136789037+Shivanash24@users.noreply.github.com>
2026-05-29 11:23:35 +0530

    fix: Vercel 404 deployment issue

---
+++ shopframe-app/shopframe/api/index.js
@@ -0,0 +1,7 @@
+import { createRequestHandler } from "@vercel/remix";
+import * as build from "../build/server/index.js";
+
+export default createRequestHandler({
+  build,
+  mode: process.env.NODE_ENV,
+});
