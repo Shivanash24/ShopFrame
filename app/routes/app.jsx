f9a46922fd23c7316d22cb730d6c1e2635c52eb9
shivansh pareek <136789037+Shivanash24@users.noreply.github.com>
2026-06-10 10:28:04 +0530

    Use anchor tags instead of Link in ui-nav-menu

--- app/routes/app.jsx
+++ app/routes/app.jsx
@@ -1,7 +1,6 @@
 import { Link, Outlet, useLoaderData, useRouteError } from "@remix-run/react";
 import { boundary } from "@shopify/shopify-app-remix/server";
 import { AppProvider } from "@shopify/shopify-app-remix/react";
-import { NavMenu } from "@shopify/app-bridge-react";
 
 import polarisStyles from "@shopify/polaris/build/esm/styles.css?url";
 import { authenticate } from "../shopify.server";
@@ -25,14 +24,14 @@ export default function App() {
 
   return (
     <AppProvider isEmbeddedApp apiKey={apiKey}>
-      <NavMenu>
-        <Link to="/app" rel="home">Dashboard</Link>
-        <Link to="/app/templates">Templates</Link>
-        <Link to="/app/customize">Customize</Link>
-        <Link to="/app/pricing">Pricing</Link>
-        <Link to="/app/settings">Settings</Link>
-        <Link to="/app/support">Support</Link>
-      </NavMenu>
+      <ui-nav-menu>
+        <a href="/app" rel="home">Dashboard</a>
+        <a href="/app/templates">Templates</a>
+        <a href="/app/customize">Customize</a>
+        <a href="/app/pricing">Pricing</a>
+        <a href="/app/settings">Settings</a>
+        <a href="/app/support">Support</a>
+      </ui-nav-menu>
       <Outlet />
     </AppProvider>
   );
