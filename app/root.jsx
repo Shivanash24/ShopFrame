ba2498f92aecaefc9acf78a04c9b08ad3c561246
shivansh pareek <136789037+Shivanash24@users.noreply.github.com>
2026-06-10 11:45:51 +0530

    Fix App Bridge initialization for ui-nav-menu

--- app/root.jsx
+++ app/root.jsx
@@ -40,6 +40,7 @@ export default function App() {
           href="https://cdn.shopify.com/static/fonts/inter/v4/styles.css"
         />
         {apiKey && <meta name="shopify-api-key" content={apiKey} />}
+        <script src="https://cdn.shopify.com/shopifycloud/app-bridge.js"></script>
         <Meta />
         <Links />
       </head>
@@ -61,6 +62,7 @@ export function ErrorBoundary() {
         <title>Oh no!</title>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <script src="https://cdn.shopify.com/shopifycloud/app-bridge.js"></script>
         <Meta />
         <Links />
       </head>
